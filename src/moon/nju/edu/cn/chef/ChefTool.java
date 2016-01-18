08919147d34f95d4de85a23f6fa11baec2f1a621
Yiqun Wang <wyqun_work@163.com>
2016-01-18 09:49:14 +0800

    fix ssl verify in jcloud 2.0.0, create sshfactory use injector

--- src/moon/nju/edu/cn/chef/ChefTool.java
+++ src/moon/nju/edu/cn/chef/ChefTool.java
@@ -8,10 +8,11 @@ import javax.inject.Singleton;
 
 import org.jclouds.Constants;
 import org.jclouds.ContextBuilder;
-import org.jclouds.chef.ChefContext;
+import org.jclouds.chef.ChefApi;
 import org.jclouds.chef.ChefService;
 import org.jclouds.chef.config.ChefProperties;
 import org.jclouds.chef.domain.BootstrapConfig;
+import org.jclouds.chef.domain.BootstrapConfig.SSLVerifyMode;
 import org.jclouds.chef.domain.CookbookVersion;
 import org.jclouds.chef.util.RunListBuilder;
 import org.jclouds.compute.domain.ExecResponse;
@@ -30,10 +31,8 @@ import com.google.inject.TypeLiteral;
 
 import moon.nju.edu.cn.demo.Server;
 
-@SuppressWarnings("deprecation")
 public class ChefTool {
 	private static ChefTool chefTool = null;
-	ChefContext chefContext = null;
 	ChefService chefService = null;
 	SshClient.Factory sshFactory = null;
 	
@@ -49,27 +48,35 @@ public class ChefTool {
 		String client = "jsmith";
     	String organization = "4thcoffee";
     	String validator = organization + "-validator";
+    	//chef-server in /etc/hosts
     	String endpoint = "https://chef-server/organizations/" + organization;
-    	String clientCredential = Files.toString(new File(System.getProperty("user.home") + "/chef-repo/.chef/" + client + ".pem"), Charsets.UTF_8);
-    	String validatorCredential = Files.toString(new File(System.getProperty("user.home") + "/chef-repo/.chef/" + validator + ".pem"), Charsets.UTF_8);
+    	String credentialPath = System.getProperty("user.home") + "/chef-repo/.chef/";
+    	String clientCredential = Files.toString(new File(credentialPath + client + ".pem"), Charsets.UTF_8);
+    	String validatorCredential = Files.toString(new File(credentialPath + validator + ".pem"), Charsets.UTF_8);
     	
     	Properties chefConfig = new Properties();
     	chefConfig.put(ChefProperties.CHEF_VALIDATOR_NAME, validator);
     	chefConfig.put(ChefProperties.CHEF_VALIDATOR_CREDENTIAL, validatorCredential);
     	chefConfig.put(Constants.PROPERTY_RELAX_HOSTNAME, "true");
+    	//skip ssl verify
     	chefConfig.put(Constants.PROPERTY_TRUST_ALL_CERTS, "true");
     	
-    	chefContext = ContextBuilder.newBuilder("chef")
+    	ChefApi chefApi= ContextBuilder.newBuilder("chef")
 				.endpoint(endpoint)
     			.credentials(client, clientCredential)
     			.overrides(chefConfig)
     			.modules(ImmutableSet.of(new SshjSshClientModule()))
-    			.buildView(ChefContext.class);
+    			.buildApi(ChefApi.class);
     	
-    	chefService = chefContext.getChefService();
+    	chefService = chefApi.chefService();
     	
-    	sshFactory = chefContext.unwrap().utils()
-    			.injector().getInstance(Key.get(new TypeLiteral<SshClient.Factory>() {}));
+    	sshFactory = ContextBuilder.newBuilder("chef")
+    			.endpoint(endpoint)
+    			.credentials(client, clientCredential)
+    			.overrides(chefConfig)
+    			.modules(ImmutableSet.of(new SshjSshClientModule()))
+    			.buildInjector()
+    			.getInstance(Key.get(new TypeLiteral<SshClient.Factory>() {}));
 	}
 	
 	public int install(Server server, String recipe) {
@@ -88,13 +95,12 @@ public class ChefTool {
 		
     	BootstrapConfig bootstrapConfig = BootstrapConfig.builder()
     			.runList(runlist)
-    			/**
-    			 * bug in 1.9.1, solved in 2.0.0
-    			 * however, we do not know how to generate ssh connection without chefcontext
-    			 * we add a line in client.rb below
-    			 */
-//    			.sslVerifyMode(SSLVerifyMode.NONE)
+    			.sslVerifyMode(SSLVerifyMode.NONE)
     			.build();
+    	
+    	/**
+    	 * @TODO change identifier of each node, here we use server.getIP()
+    	 */
     	chefService.updateBootstrapConfigForGroup(server.getIP(), bootstrapConfig);
     	Statement bootstrap = chefService.createBootstrapScriptForGroup(server.getIP());
     	
@@ -114,9 +120,9 @@ public class ChefTool {
     		rawScript = "sudo chmod 777 /etc/chef/client.rb \n" + rawScript;
     		
     		//need ssl_verify_mode to be none, bug in 1.9.1
-    		rawScript = rawScript.replace("chef-client -j /etc/chef/first-boot.json", 
-    				"echo \"ssl_verify_mode :verify_none\" >> /etc/chef/client.rb \n"
-    				+ "sudo chef-client -j /etc/chef/first-boot.json \n");
+//    		rawScript = rawScript.replace("chef-client -j /etc/chef/first-boot.json", 
+//    				"echo \"ssl_verify_mode :verify_none\" >> /etc/chef/client.rb \n"
+//    				+ "sudo chef-client -j /etc/chef/first-boot.json \n");
     		
 //    		System.out.println("Raw script rendered.\n\n" + rawScript + "\n\n");
     		System.out.println("Bootstrap script executed...");
@@ -125,8 +131,8 @@ public class ChefTool {
     		 * the shell might not succeed during one process, that is the chef problem
     		 * u can run "sudo chef-client -j /etc/chef/first-boot.json" until it is successful
     		 */
-    		ExecResponse result = sshClient.exec(rawScript);
-    		System.out.println(result.toString());
+//    		ExecResponse result = sshClient.exec(rawScript);
+//    		System.out.println(result.toString());
     		
     		/**
     		 * another solution
